2f49d3162bb74e264f015d95a361c7ed774ffdc0
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-05-13 21:54:07 -0500

    Added schemas

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+//Apollo Server requires wrapping your schema with gql.
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input bookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: bookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+//import ApolloServer
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
